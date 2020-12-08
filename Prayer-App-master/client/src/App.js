bb5f838cc376aace2ae029b8a2221e70fac90d4f
thelaing7 <61952549+thelaing7@users.noreply.github.com>
2020-12-08 14:13:39 -0800

    Update App.js

--- Prayer-App-master/client/src/App.js
+++ Prayer-App-master/client/src/App.js
@@ -1,32 +1,81 @@
 
 import './App.css';
-import { useState } from "react"; 
+import { useState } from "react";
 import Axios from "axios";
 
 
 function App() {
-  const [PrayerRequest, setPrayerRequest ] = useState("");
-  const [Comments, setComments ] = useState("");
 
- 
-const addPrayerRequest = () => {
-   Axios.post('http://localhost:3001/create',{PrayerRequest:PrayerRequest,Comments:Comments}).then(()=> {
-     console.log("success");
-   });
-};
+  const [Comments, setComments] = useState("");
+
+  const [PrayerRequest, setPrayerRequest] = useState([]);
+
+  const [newPrayerRequest, setNewPrayerRequest] = useState("");
+
+
+  function addPrayerRequest() {
+    Axios.post('http://localhost:3000/create', { PrayerRequest: PrayerRequest, Comments: Comments }).then(() => {
+      console.log("success");
+    });
+  }
+
+  const getPrayerRequest = () => {
+    Axios.get('http://localhost:3000/prayer').then((response) => {
+      setPrayerRequest(response.data);
+    });
+  }
+
+  const updatePrayerRequest = (id) => {
+    Axios.put('http://localhost:3000/update', { PrayerRequest: newPrayerRequest, id: id }).then(
+      (response) => {
+        alert("Updated PrayerRequest")
+      }
+    );
+  };
+  function DeletePrayerRequest(id) {
+    Axios.delete(`http://localhost:3000/delete/${id}`);
+  }
+
+
+
 
   return (
     <div className="App">
       <div className="information">
-      <label>Prayer Request:</label>
-      <input type="text" onChange={(Event) => {setPrayerRequest(Event.target.value);}}/>
-      <label>Comments:</label>
-      <input type="text"  onChange={(Event) => {setComments(Event.target.value);}}/>
-      <button onClick={addPrayerRequest}>Add Prayer </button>
+        <label>Prayer Request:</label>
+        <input type="text" onChange={(Event) => { setPrayerRequest(Event.target.value); }} />
+        <label>Comments:</label>
+        <input type="text" onChange={(Event) => { setComments(Event.target.value); }} />
+        <button onClick={addPrayerRequest}>Add PrayerRequest </button>
+        <br></br>
+      </div>
+      <div className="PrayerRequest">
+        <button onClick={getPrayerRequest}>Show PrayerRequest</button>
+
+        {PrayerRequest.map((val, key) => {
+          return (
+            <div className="PrayerRequest">
+
+              <div><input
+                type="text"
+                placeholder="PRAYER UPDATE"
+                onChange={(Event) => {
+                  setNewPrayerRequest(Event.target.value);
+                }}
+              />
+                <button onClick={() => { updatePrayerRequest(val.id); }} >{" "}Update PrayerRequest</button>
+
+                <button onClick={() => { DeletePrayerRequest(val.id) }}>DeletePrayerRequest</button>
+              </div>
+            </div>
+
+
+          );
+        })}
       </div>
     </div>
   );
-  
+
 }
 
 export default App;
